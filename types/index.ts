bb71ca3865ad06869388fab34968e8010bc86ec9
Maxisan <maxisanchez19@gmail.com>
2023-03-06 10:26:45 +0000

    add SocialMedia interface

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,10 @@ import { ReactNode } from "react"
 export type WithChildren = {
   children: ReactNode
 }
+
+export interface ISocialMedia {
+  id: string;
+  name: string;
+  icon: string;
+  url: string;
+}
