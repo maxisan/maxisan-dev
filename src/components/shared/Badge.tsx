7fb3d6e3dcd0d486bfb81b24c6797dc263a43301
Maxisan <maxisanchez19@gmail.com>
2023-03-16 12:15:14 +0000

    Add shared component Badge

---
+++ src/components/shared/Badge.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Badge = styled.div`
+  font-size: 0.8rem;
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+`
+
+export default Badge
